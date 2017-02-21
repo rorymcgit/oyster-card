95f936c6b62e8f99d83c72856be3600ad9b0e7e4
Ryan <Ryanchu.ws@gmail.com>
2017-02-21 16:14:51 +0000

    Step 8 completed, working on step 9 => Seeing if instance variables are redundant (touched in vs in journey)

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,15 +1,40 @@
 
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :touched_in, :in_journey
 
   MAX_MONEY = 90
+  MIN_MONEY = 1
 
   def initialize
     @balance = 0
+    @touched_in = false
+    @in_journey = false
   end
 
   def topup(value)
     fail "Your balance cannot go over £#{Oystercard::MAX_MONEY}." if @balance + value > MAX_MONEY
     @balance += value
   end
+
+  def deduct(fare)
+    @balance -= fare
+  end
+
+  def touch_in
+    fail "min. balance of £#{Oystercard::MIN_MONEY} not reached" if @balance <= MIN_MONEY
+    @touched_in = true
+  end
+
+  def touch_out
+    @touched_in = false
+  end
+
+  def in_journey?
+    if @touched_in == true
+      @in_journey = true
+    else
+      @in_journey = false
+    end
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -31,4 +31,56 @@ end
 
   end
 
+  context "#deduct" do
+    it "responds to deduct with 1 argument" do
+      expect(subject).to respond_to(:deduct).with(1).argument
+    end
+
+    it "deducts fare from balance" do
+      expect{subject.deduct(10)}.to change{ subject.balance }.by -10
+    end
+
+  end
+
+  context "#touch_in" do
+    before(:each) do
+      subject.topup(50)
+    end
+
+    it "is initially not in a journey" do
+      expect(subject).not_to be_in_journey
+    end
+
+    it "responds to touch_in method" do
+      expect(subject).to respond_to(:touch_in)
+    end
+
+    it "registers as being in a journey after touchin" do
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+  end
+
+context "#touch_in_errors" do
+    it "raises an error when minimum amount not reached" do
+      expect{ subject.touch_in }.to raise_error "min. balance of £#{Oystercard::MIN_MONEY} not reached"
+    end
+  end
+
+context "#touch_out" do
+  before(:each) do
+    subject.topup(50)
+  end
+
+  it "responds to touch_out method" do
+    expect(subject).to respond_to(:touch_out)
+  end
+
+  it "registers as journey complete after touchout" do
+    subject.touch_in
+    subject.touch_out
+    expect(subject).not_to be_in_journey
+  end
+end
+
 end
