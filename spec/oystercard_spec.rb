f2de3a4932e35bfb2fe456374fe92c18da6cb612
Ryan <Ryanchu.ws@gmail.com>
2017-02-21 12:16:30 +0000

    cleaned up tests, learnt to change syntax

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,21 +2,28 @@ require "oystercard"
 
 describe Oystercard do
 
+  context "#balance" do
   it "carries a balance" do
     expect(subject.balance).to eq 0
   end
 
-  describe "#topup" do
+  it "has an effect on the balance" do
+    expect{ subject.topup(10)}.to change{ subject.balance }.by 10
+  end
+
+end
+
+  context "#topup" do
+
+    it "responds to topup with 1 argument" do
+      expect(subject).to respond_to(:topup).with(1).argument
+    end
+
+
     it "allows topup with a value" do
       expect(subject.topup(20)).to eq 20
     end
 
-    it "has an effect on the balance" do
-      subject.topup(15)
-      expect(subject.balance).to eq 15
-      subject.topup(15)
-      expect(subject.balance).to eq 30
-    end
 
     it "will not topup when the balance would be over £#{Oystercard::MAX_MONEY}" do
       expect{subject.topup(91)}.to raise_error "Your balance cannot go over £#{Oystercard::MAX_MONEY}."
